7555257904962ae3e51457033426bc5eadf74f01
Mark Hughes <markscfchughes@gmail.com>
2025-05-28 23:19:50 +0100

    Debug: log UK hour from Luxon

--- pages/api/chat.ts
+++ pages/api/chat.ts
@@ -27,6 +27,7 @@ if (userMessage === "👋 INITIATE") {
   // Get UK time using Luxon
   const ukTime = DateTime.now().setZone("Europe/London");
   const hour = ukTime.hour;
+  console.log("🔍 UK Hour (Luxon):", hour);
 
   let greeting = "Good afternoon";
   if (hour < 12) greeting = "Good morning";
